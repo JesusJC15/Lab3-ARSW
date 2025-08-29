c0c5b3aa320dda95d7e04e326bdbc68fbfdb41e9
unknown <jesus.jauregui-c@labinfo.is.escuelaing.edu.co>
2025-08-29 08:58:21 -0500

    Black list search imported

---
+++ src/main/java/edu/eci/arsw/black_list_search/blacklistvalidator/HostBlackListsValidator.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.black_list_search.blacklistvalidator;
+
+import edu.eci.arsw.black_list_search.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class HostBlackListsValidator {
+
+    private static final int BLACK_LIST_ALARM_COUNT=5;
+    
+    /**
+     * Check the given host's IP address in all the available black lists,
+     * and report it as NOT Trustworthy when such IP was reported in at least
+     * BLACK_LIST_ALARM_COUNT lists, or as Trustworthy in any other case.
+     * The search is not exhaustive: When the number of occurrences is equal to
+     * BLACK_LIST_ALARM_COUNT, the search is finished, the host reported as
+     * NOT Trustworthy, and the list of the five blacklists returned.
+     * @param ipaddress suspicious host's IP address.
+     * @return  Blacklists numbers where the given host's IP address was found.
+     */
+    public List<Integer> checkHost(String ipaddress, int nThreads){
+        HostBlacklistsDataSourceFacade skds=HostBlacklistsDataSourceFacade.getInstance();
+        int totalServers = skds.getRegisteredServersCount();
+
+        int serversPerThread = totalServers / nThreads;
+        List<HostBlackListThread> threads = new LinkedList<>();
+        
+        for(int i=0; i<nThreads; i++){
+            int start = i * serversPerThread;
+            int end = (i == nThreads - 1) ? totalServers : start + serversPerThread;
+            HostBlackListThread thread = new HostBlackListThread(ipaddress, start, end, skds);
+            threads.add(thread);
+            thread.start();
+        }
+        
+        List<Integer> blackListOcurrences = new LinkedList<>();
+        int checkedListsCount = 0;
+        for(HostBlackListThread thread : threads) {
+            try {
+                thread.join();
+                blackListOcurrences.addAll(thread.getLocalOcurrences());
+                checkedListsCount += thread.getCheckedLists();
+            } catch (InterruptedException ex) {
+                LOG.log(Level.SEVERE, "Thread interrupted", ex);
+            }
+        }
+
+        if (blackListOcurrences.size() >= BLACK_LIST_ALARM_COUNT) {
+            skds.reportAsNotTrustworthy(ipaddress);
+        } else {
+            skds.reportAsTrustworthy(ipaddress);
+        }
+        
+        LOG.log(Level.INFO, "Checked Black Lists:{0} of {1}", new Object[]{checkedListsCount, totalServers});
+        
+        return blackListOcurrences;
+    }
+    
+    private static final Logger LOG = Logger.getLogger(HostBlackListsValidator.class.getName());    
+    
+}
---
+++ src/main/java/edu/eci/arsw/black_list_search/blacklistvalidator/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.black_list_search.blacklistvalidator;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Main {
+    
+    public static void main(String a[]){
+        HostBlackListsValidator hblv=new HostBlackListsValidator();
+        
+        System.out.println("IP: 200.24.34.55");
+        hblv.checkHost("200.24.34.55", 10);
+
+        System.out.println("IP: 202.24.34.55");
+        hblv.checkHost("202.24.34.55", 10);
+
+        System.out.println("IP: 212.24.24.55");
+        hblv.checkHost("212.24.24.55", 10);
+        
+    }
+    
+}
